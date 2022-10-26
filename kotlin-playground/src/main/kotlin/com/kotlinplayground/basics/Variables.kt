43f487646847e6a1d88d45bc9651f9eb7ab50525
EMANUEL DIAZ <emadiaz@santandertecnologia.com.ar>
2022-10-26 03:49:15 -0300

    10. Basic Types - Int, Long, Double,

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -8,4 +8,23 @@ fun main() {
     println(age)
     age = 34
     println(age)
+
+    //Type Inference
+    var salary = 43000L
+    println(salary)
+
+    //Strin interpolation
+    var course = "Kotlin Spring"
+    println("course: $course and course length is ${course.length}")
+
+    var multiline = "AND \n DEF"
+    println(multiline)
+
+    var multiline1 = """"
+    ABD
+    DEF
+    """.trimIndent()
+    println(multiline1)
+
+
 }
